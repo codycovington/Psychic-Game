7b251dd19265dcdc395732a170de3648aa101fc1
codycovington <codebase.covington@gmail.com>
2018-06-04 09:09:12 -0400

    "Initializing files"

---
+++ assets/javascript/game.js
@@ -0,0 +1,75 @@
+// Initialize game counters to zero
+var numWins = 0;
+var numLosses = 0;
+var guessLimit = 9;
+
+
+// Setup the letter array and lettersGuessed[empty] array
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var lettersGuessed =[];
+
+
+// Begin event listening function
+document.onkeyup = function(event) {
+    
+    
+// Computer guesses a random letter from the letters array and logs it
+var computerGuess = letters[Math.floor(Math.random()*letters.length)];
+console.log("Computer chose " + computerGuess);
+
+
+// Accept user input, push pressed letter to the lettersGuessed array and decrement the guessLimit
+var userGuess = event.key;
+
+lettersGuessed.push(userGuess);
+
+guessLimit--;
+
+// Determine the outcome and invoke RESET function to clear counters and array
+
+if (userGuess === computerGuess) {
+    alert("YOU WIN!!!");
+    numWins++;
+
+    function reset() {
+        guessLimit = 9;
+        lettersGuessed.length = 0;
+
+    document.querySelector("#game").innerHTML = html;
+        
+    };
+    
+    reset();
+   
+}
+
+if (guessLimit === 0) {
+    alert("YOU LOSE!!!");
+    numLosses++;
+
+    function reset() {
+        guessLimit = 9;
+        lettersGuessed.length = 0;
+
+    document.querySelector("#game").innerHTML = html;
+        
+    };
+
+    reset();
+}
+
+
+//Display html and add in JS elements as game progresses
+var html = 
+"<br>" +
+"<p>Wins:  " + "<strong>" + numWins + "</strong>" + "</p>" +
+"<p>Losses:  " + "<strong>" + numLosses + "</strong>" + "</p>" +
+"<p>Letters guessed:  " + "<strong>" + lettersGuessed + "</strong>" + "</p>" +
+"<p>Number of guesses left:  " + "<strong>" + guessLimit + "</strong>" + "</p>";
+
+document.querySelector("#game").innerHTML = html;
+
+
+
+};
